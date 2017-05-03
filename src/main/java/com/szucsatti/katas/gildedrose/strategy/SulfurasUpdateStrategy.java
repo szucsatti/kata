58b9b8658c912cd5beeb8e5bd35f8a7dd7f5137b
szucsa <szucsatti87@gmail.com>
2017-05-03 20:04:26 +0300

    SulfurasUpdateStrategy added

---
+++ src/main/java/com/szucsatti/katas/gildedrose/strategy/SulfurasUpdateStrategy.java
@@ -0,0 +1,12 @@
+package com.szucsatti.katas.gildedrose.strategy;
+
+import com.szucsatti.katas.gildedrose.Item;
+
+public class SulfurasUpdateStrategy implements UpdateStrategy {
+
+	@Override
+	public void updateQuality(final Item item) {
+		// literally do nothing
+	}
+
+}
