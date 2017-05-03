7da41698e23062f96444de81c45ea477ac2d9998
szucsa <szucsatti87@gmail.com>
2017-05-03 18:38:19 +0300

    TestBase created
    Base class for tests created

--- src/test/java/com/szucsatti/katas/gildedrose/GildedRoseTest.groovy
+++ src/test/java/com/szucsatti/katas/gildedrose/GildedRoseTest.groovy
@@ -2,27 +2,7 @@ package com.szucsatti.katas.gildedrose;
 
 import org.junit.Test;
 
-public class GildedRoseTest {
-
-	final int ZERO = 0
-
-	final int ONE = 1
-
-	final int TWO = 2
-
-	final int THREE = 3
-	
-	final int FOUR = 4
-
-	final int FIVE = 5
-	
-	final int NINE = 9
-	
-	final int TEN = 10
-	
-	final int ELEVEN = 11
-
-	final int MAX_QUALITY = 50
+public class GildedRoseTest extends TestBase {
 
 
 	@Test
---
+++ src/test/java/com/szucsatti/katas/gildedrose/TestBase.groovy
@@ -0,0 +1,24 @@
+package com.szucsatti.katas.gildedrose
+
+class TestBase {
+
+	final int ZERO = 0
+
+	final int ONE = 1
+
+	final int TWO = 2
+
+	final int THREE = 3
+
+	final int FOUR = 4
+
+	final int FIVE = 5
+
+	final int NINE = 9
+
+	final int TEN = 10
+
+	final int ELEVEN = 11
+
+	final int MAX_QUALITY = 50
+}
