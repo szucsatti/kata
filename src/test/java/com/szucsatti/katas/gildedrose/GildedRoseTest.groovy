9bb1348764d0618c54962aedea5ab238308e0c0d
szucsa <szucsatti87@gmail.com>
2017-05-03 19:23:24 +0300

    Typo corrected

--- src/test/java/com/szucsatti/katas/gildedrose/GildedRoseTest.groovy
+++ src/test/java/com/szucsatti/katas/gildedrose/GildedRoseTest.groovy
@@ -46,7 +46,7 @@ public class GildedRoseTest extends TestBase {
 
 	
 	@Test
-	public void "aged brie's quality quality increases by two afer sell by date passes" (){
+	public void "aged brie's quality increases by two afer sell by date passes" (){
 		def items = [ new Item("Aged Brie", ZERO, ONE) ] as Item[]
 		def classToTest = new GildedRose(items)
 		classToTest.updateQuality()
